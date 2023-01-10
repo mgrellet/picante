9e37a41b41739a0714514e061b5ea84e88ecf753
Martin Grellet <martin.alejandro.grellet@gmail.com>
2023-01-10 04:05:23 -0300

    0052 - export invoice to pdf

--- src/app/components/invoice-dialog/invoice-dialog.component.ts
+++ src/app/components/invoice-dialog/invoice-dialog.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import jsPDF from 'jspdf';
+import html2canvas from 'html2canvas';
 
 @Component({
   selector: 'app-invoice-dialog',
@@ -7,7 +9,29 @@ import { Component } from '@angular/core';
 })
 export class InvoiceDialogComponent {
 
-  sendInvoice() {
+  downloadPdf() {
+    const DATA = document.getElementById('invoice');
+    const doc = new jsPDF('p', 'pt', 'a4');
+    const options = {
+      background: 'white',
+      scale: 3
+    };
+    // @ts-ignore
+    html2canvas(DATA, options).then((canvas) => {
+
+      const img = canvas.toDataURL('image/PNG');
+
+      // Add image Canvas to PDF
+      const bufferX = 15;
+      const bufferY = 15;
+      const imgProps = (doc as any).getImageProperties(img);
+      const pdfWidth = doc.internal.pageSize.getWidth() - 2 * bufferX;
+      const pdfHeight = (imgProps.height * pdfWidth) / imgProps.width;
+      doc.addImage(img, 'PNG', bufferX, bufferY, pdfWidth, pdfHeight, undefined, 'FAST');
+      return doc;
+    }).then((docResult) => {
+      docResult.save(`${new Date().toISOString()}_tutorial.pdf`);
+    });
 
   }
 }
